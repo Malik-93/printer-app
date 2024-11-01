e623321ae29202e2ae565ab19afdf935f9694f18
Mudassir Malik <Malikjrw147@gmail.com>
2024-11-01 21:33:54 +0500

    removed old printer app

--- config.ts
+++
@@ -1,9 +0,0 @@
-import * as dotenv from "dotenv";
-dotenv.config();
-export const config = {
-  addServerHttp: process.env.ADD_SERVER_HTTP,
-  storeEmail: process.env.STORE_EMAIL ?? "",
-  env: process.env.NODE_ENV ?? "development",
-  port: process.env.PORT ?? "9000",
-  PROJECT: process.env.PROJECT ?? "Printer app",
-};
--- router/index.ts
+++
@@ -1,8 +0,0 @@
-import express, { Router, Request, Response } from 'express';
-import printerRoutes from './printer.routes';
-const router: Router = express.Router();
-router.get("/", (_req: Request, _res: Response) => {
-    return _res.status(200).json({ active: true, message: 'Printer app is up and running...' })
-});
-router.use('/api/printer', printerRoutes);
-export = router;
