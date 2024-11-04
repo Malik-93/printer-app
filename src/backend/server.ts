0e02fc1706ae33be5626e87fc0dbec927fe441ef
Mudassir Malik <Malikjrw147@gmail.com>
2024-11-04 17:03:37 +0500

    Save ngrok url to database

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -5,46 +5,36 @@ import PrinterRouter from "./router/printer.routes";
 import PrinterController from "./controllers/printer.controller";
 import path from "path";
 import errorHandler from "./middlewares/error.handler";
-import http from "http";
 import { _globals, isDev, root_dir } from "./constants";
 import Ngrok from "./ngrok/Ngrok";
 import logger from "./logger";
 import { config } from "./config";
+import axios from "axios";
 export default class LocalServer {
   private app: Express;
   private printerRouter;
   private printerController;
+  private ADD_SERVER_HTTP;
 
   constructor() {
+    process.stdin.resume();
+    process.on("SIGINT", async () => {
+      await Ngrok.kill();
+      process.exit();
+    });
+
+    process.on("uncaughtException", (error: Error) => {
+      console.log("[uncaughtException]", error);
+      logger.info("[uncaughtException]", error);
+    });
+
+    this.ADD_SERVER_HTTP = config.addServerHttp;
     this.printerController = new PrinterController();
     const printerRoutes = new PrinterRouter(this.printerController);
     this.printerRouter = printerRoutes.getRouter();
     this.app = express();
     this.setupMiddleware();
     this.setupRoutes();
-  }
-
-  start(port: number) {
-    this.app.listen(port, "localhost", async () => {
-      try {
-        const server_url = `http://localhost:${port}`;
-        const ngrok_url = (await Ngrok.init()) as string;
-        const ngrok_version = await Ngrok.getVersion();
-        console.log(
-          `\x1b[35m Printer app is up and running... \x1b[0m \n\n \x1b[34mLocalUrl:\x1b[0m ${server_url} \n\n \x1b[35mNgrokUrl:\x1b[0m ${ngrok_url} \n\n \x1b[32mEnvironment:\x1b[0m ${config.env} \n\n \x1b[35mRootDirectory:\x1b[0m ${root_dir} \n\n \x1b[32mApp Version:\x1b[0m 1.2.0 \n\n \x1b[32mNgrok Version:\x1b[0m ${ngrok_version} \n\n`
-        );
-        logger.info(
-          `Printer app is up and running on \n url: ${server_url}, environment: ${config.env}, rootDirectory: ${root_dir}`
-        );
-        if (ngrok_url) {
-          _globals.ngrok_url = ngrok_url;
-          // await sendNgrokUrl(ngrok_url);
-        }
-      } catch (error) {
-        if (typeof error === "object") error = JSON.stringify(error);
-        console.log(`\x1b[31m Server couldn't start ${error}\x1b[0m`);
-      }
-    });
   }
 
   private setupMiddleware() {
@@ -63,5 +53,63 @@ export default class LocalServer {
     });
     this.app.use("/uploads", express.static(path.join(__dirname, "uploads")));
     this.app.use("/api/printer", this.printerRouter);
+  }
+
+  private async sendNgrokUrl(ngrokUrl: string) {
+    try {
+      const storeEmail = config.storeEmail;
+      _globals.store_email = config.storeEmail;
+
+      console.log(`Current email: ${storeEmail} \n`);
+      console.log(`\x1b[33m Saving ngrok url into database... \x1b[0m \n`);
+
+      const response = await axios.post(
+        `${this.ADD_SERVER_HTTP}`,
+        {
+          servers: [
+            {
+              Email: storeEmail,
+              Url: ngrokUrl,
+            },
+          ],
+        },
+        { headers: { "Content-Type": "application/json" } }
+      );
+
+      console.log(
+        `\x1b[33m ${response.status} - ${response.statusText} \x1b[0m \n`
+      );
+    } catch (error) {
+      console.log(
+        `\x1b[31m An error occurred while saving ngrok url into database! ${error} \x1b[0m \n`
+      );
+    }
+  }
+
+  start(port: number) {
+    this.app.listen(port, "localhost", async () => {
+      try {
+        const server_url = `http://localhost:${port}`;
+        const ngrok_url =
+          !isDev && process.platform === "darwin"
+            ? ""
+            : ((await Ngrok.init()) as string);
+
+        const ngrok_version = await Ngrok.getVersion();
+        console.log(
+          `\x1b[35m ${name} is up and running... \x1b[0m \n\n \x1b[34mLocalUrl:\x1b[0m ${server_url} \n\n \x1b[35mNgrokUrl:\x1b[0m ${ngrok_url} \n\n \x1b[32mEnvironment:\x1b[0m ${config.env} \n\n \x1b[35mRootDirectory:\x1b[0m ${root_dir} \n\n \x1b[32mApp Version:\x1b[0m 1.2.0 \n\n \x1b[32mNgrok Version:\x1b[0m ${ngrok_version} \n\n`
+        );
+        logger.info(
+          `${name} is up and running on \n url: ${server_url}, environment: ${config.env}, rootDirectory: ${root_dir}`
+        );
+        if (ngrok_url) {
+          _globals.ngrok_url = ngrok_url;
+          await this.sendNgrokUrl(ngrok_url);
+        }
+      } catch (error) {
+        if (typeof error === "object") error = JSON.stringify(error);
+        console.log(`\x1b[31m Server couldn't start ${error}\x1b[0m`);
+      }
+    });
   }
 }
