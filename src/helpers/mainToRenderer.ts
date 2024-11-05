9f309a44652076211a421faf20d1b4aa57a93b8a
Mudassir Malik <Malikjrw147@gmail.com>
2024-11-05 22:21:00 +0500

    Revert "mdnssd added"
    This reverts commit 4427e892fe9bc8396c35a10abf804660ddec132f.

--- src/helpers/mainToRenderer.ts
+++ src/helpers/mainToRenderer.ts
@@ -1,32 +1,8 @@
 import { BrowserWindow, Menu, app } from "electron";
-// @ts-ignore
-import mDnsSd from "node-dns-sd";
 export default async (mainWindow: BrowserWindow) => {
   const getPrintersAsync = async (): Promise<void> => {
     const printers = await mainWindow.webContents.getPrintersAsync();
-    console.log("printers", printers);
-    mDnsSd
-      .discover({ name: "_printer._tcp.local" })
-      .then((device_list: any[]) => {
-        console.log("device_list", JSON.stringify(device_list, null, "  "));
-        mainWindow.webContents.send("on-printers", device_list);
-      })
-      .catch((error: any) => {
-        console.error(error);
-      });
-
-    mDnsSd.ondata = (packet: any) => {
-      console.log(JSON.stringify(packet, null, "  "));
-    };
-
-    mDnsSd
-      .startMonitoring()
-      .then(() => {
-        console.log("Started.");
-      })
-      .catch((error: any) => {
-        console.error(error);
-      });
+    mainWindow.webContents.send("on-printers", printers);
   };
 
   mainWindow.webContents.on(
